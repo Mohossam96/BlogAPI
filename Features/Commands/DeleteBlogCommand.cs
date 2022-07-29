5b5c01066e393c60b279e732d941d32483b7a46e
Mohossam <mohamed.hosam@el-eng.menofia.edu.eg>
2022-07-29 10:45:05 +0200

    Add project files.

---
+++ Features/Commands/DeleteBlogCommand.cs
@@ -0,0 +1,31 @@
+﻿using BlogAPI.Context;
+using BlogAPI.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Features.Commands
+{
+    public class DeleteBlogCommand :IRequest<int>
+    {
+        public int Id { get; set; }
+        public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand,int>
+        {
+            private readonly Application_Context _context;
+            public DeleteBlogCommandHandler(Application_Context context)
+            {
+                _context = context;
+            }
+            public async Task<int> Handle(DeleteBlogCommand command,CancellationToken cancellation)
+            {
+                var blog = await _context.Blogs.Where(x => x.Id == command.Id).FirstOrDefaultAsync();
+                if (blog == null) return default;
+                _context.Blogs.Remove(blog);
+                await _context.SaveChanges();
+                return blog.Id;
+            }
+        }
+    }
+}
---
+++ Models/blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlogAPI.Models
+{
+    public class blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate  { get; set; } = DateTime.Now;
+
+    }
+}
